528c6e452c81e4e5047c8b922800df7602f83f76
fdtlyy <2731696875@qq.com>
2024-11-30 10:59:27 +0800

    初识循环语句

---
+++ 循环语句.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int line = 0;
+	while (line < 3000)
+	{
+		printf("Ð´´úÂë:%d\n",line);
+		line++;
+	}
+	if (line == 3000)
+	{
+		printf("ºÃoffer\n");
+	}
+	return 0;
+}
