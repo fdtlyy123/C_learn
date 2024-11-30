6052dcca37fbcf11771ebec9b87d8a54521e3adc
fdtlyy <2731696875@qq.com>
2024-11-30 10:57:19 +0800

    初识选择语句

---
+++ 选择语句.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main()
+//{
+//	int input = 0;//输入的值
+//	printf("加入比特:>");
+//	printf("那要好好学习吗(1/0)?>:");
+//	scanf("%d", &input);//&input表示地址列表，中的地址是接受数据变量的地址
+//
+//
+//	if (input == 1)
+//	{
+//		printf("好offer\n");
+//	}
+//	else
+//	{
+//		printf("卖红薯\n");
+//	}
+//	return 0;
+//}
